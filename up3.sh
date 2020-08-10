412ae64df4a85267d0b6a596e5f32edaa426c1a1
AkasakaID <55008381+akasakaid@users.noreply.github.com>
2020-08-10 18:18:16 +0700

    Update up3.sh

--- up3.sh
+++ up3.sh
@@ -1,4 +1,1 @@
-apt-get install python3.7
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 2
-sudo update-alternatives --config python3
+sudo apt-get install python3.8 && sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1 && sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 2 && sudo update-alternatives --config python3
