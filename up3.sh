dc1fb93415ff142dbe3c34b1cea836eb213f6388
AkasakaID <55008381+akasakaid@users.noreply.github.com>
2020-10-13 11:08:58 +0700

    Update and rename up3.sh to updateto3.8.sh

--- up3.sh
+++
@@ -1,1 +0,0 @@
-sudo apt-get install python3.8 && sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1 && sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 2 && sudo update-alternatives --config python3
