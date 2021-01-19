954054ffed77715417b29cb6431b610e49ef193a
AkasakaID <55008381+akasakaid@users.noreply.github.com>
2021-01-19 23:23:14 +0700

    udpate

--- 3.8.sh
+++ 3.8.sh
@@ -1,3 +1,5 @@
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
 sudo apt-get install python3.8
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 2
--- 3.9.sh
+++ 3.9.sh
@@ -1,3 +1,5 @@
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
 sudo apt-get install python3.9
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 2
