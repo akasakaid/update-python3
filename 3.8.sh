1d7f325f3bca0e71723916b593de5907f7402f82
AkasakaID <55008381+akasakaid@users.noreply.github.com>
2021-01-19 23:20:43 +0700

    update

---
+++ 3.8.sh
@@ -0,0 +1,4 @@
+sudo apt-get install python3.8
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 2
+sudo update-alternatives --config python3
---
+++ 3.9.sh
@@ -0,0 +1,4 @@
+sudo apt-get install python3.9
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 2
+sudo update-alternatives --config python3
