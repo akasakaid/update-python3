5cb00b4795d8d73f25da30e472710f816a35b73e
AkasakaID <55008381+akasakaid@users.noreply.github.com>
2021-01-19 23:53:03 +0700

    update

--- 3.8.sh
+++ 3.8.sh
@@ -1,6 +1,7 @@
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update
 sudo apt-get install python3.8
+sudo apt install python3.8-distutils
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 2
 sudo update-alternatives --config python3
--- 3.9.sh
+++ 3.9.sh
@@ -1,6 +1,7 @@
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update
 sudo apt-get install python3.9
+sudo apt install python3.9-distutils
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 2
 sudo update-alternatives --config python3
